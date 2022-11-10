c27283201af52d2a6c2faf92c34bf38c0241bfc4
Hypersand <109746210+Hypersand@users.noreply.github.com>
2022-11-10 17:47:23 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,9 +1,13 @@
+
+// int[] newArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1])
+// Arrays.copyOfRange(원본 배열,복사하려는 시작 요소의 인덱스,복사하려는 마지막 요소의 인덱스의 바로 다음 인덱스)
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int [] answer = new int[commands.length];
-        
+
         for(int i = 0; i<commands.length; i++) {
             int [] newArray = new int[commands[i][1] - commands[i][0] + 1];
             int k = commands[i][0] - 1;
@@ -14,8 +18,8 @@ class Solution {
             Arrays.sort(newArray);
             answer[i] = newArray[commands[i][2]-1];
         }
-        
-        
+
+
         return answer;
     }
 }
