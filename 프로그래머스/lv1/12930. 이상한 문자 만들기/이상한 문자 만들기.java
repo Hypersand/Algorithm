4d00e1f1cc530f95f05422bca9b5d06f18ba682b
Hypersand <109746210+Hypersand@users.noreply.github.com>
2022-11-12 16:22:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.51 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String s) {
+        String [] arr = s.split(" ", -1);
+        String answer = "";
+        
+        for(int i = 0; i<arr.length; i++) {
+            String [] arr2 = arr[i].split("");
+            for(int j = 0; j<arr2.length; j++) {
+                if(j%2==0) {
+                    answer += arr2[j].toUpperCase();
+                }
+                else {
+                    answer += arr2[j].toLowerCase();
+                }
+                }
+            
+            if(i == arr.length-1)
+                break;
+            
+            answer += " ";
+            }
+        
+        
+        return answer;
+        
+        }
+    }
