cd6e31e6c1e94d8e8e14d2cacd7ce046c06e87f8
Hypersand <109746210+Hypersand@users.noreply.github.com>
2022-11-12 16:40:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.36 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -8,22 +8,25 @@ class Solution {
         for(int i = 0; i<arr.length; i++) {
             String [] arr2 = arr[i].split("");
             
-                for (int j = 0; j<arr2.length; j++) {
-                    if(j == 0) {
-                        answer += arr2[j].toUpperCase();
-                    }
-                    else {
-                        answer += arr2[j].toLowerCase();
-                    }
-                }
-                    
-        
-            
-            if(i<arr.length-1) {
-                answer += " ";
+            if(arr[i].length()>1) {
+                answer += arr[i].substring(0,1).toUpperCase();
+                answer += arr[i].substring(1,arr[i].length()).toLowerCase();
             }
-        
-              
+            else {
+                answer += arr[i].substring(0).toUpperCase();
+            }
+            
+                // for (int j = 0; j<arr2.length; j++) {
+                //     if(j == 0) {
+                //         answer += arr2[j].toUpperCase();
+                //     }
+                //     else {
+                //         answer += arr2[j].toLowerCase();
+                //     }
+                // }
+                    
+            if(i<arr.length-1) 
+                answer += " ";
          }
         return answer;
     }
