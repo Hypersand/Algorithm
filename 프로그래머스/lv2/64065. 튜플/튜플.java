dfb81950083f7c436c04062b331a04482f472d6b
Hypersand <109746210+Hypersand@users.noreply.github.com>
2022-12-19 16:59:37 +0900

    [level 2] Title: 튜플, Time: 0.37 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        List<Integer> list = new ArrayList<>();
+        s = s.substring(2,s.length());
+        s = s.substring(0,s.length()-2).replace("},{","-");
+        String [] arr = s.split("-");
+        
+        Arrays.sort(arr,new Comparator<String>(){
+            public int compare(String o1, String o2){
+                
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        
+        for(String str : arr) {
+            String [] tmp;
+            tmp = str.split(",");
+            
+            for(int j = 0 ; j < tmp.length;j++){
+                int n = Integer.parseInt(tmp[j]);
+                if(!list.contains(n))  list.add(n);
+            }
+        }
+        
+        int [] answer = new int[list.size()];
+        for(int k = 0; k<list.size(); k++) {
+	        answer[k] = list.get(k).intValue();
+        }        
+        return answer;
+    }
+}
