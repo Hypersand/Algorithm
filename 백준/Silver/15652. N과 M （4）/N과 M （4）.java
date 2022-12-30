9ebb92b69a83419ba3c505fcebcb7b9e56842f37
Hypersand <109746210+Hypersand@users.noreply.github.com>
2022-12-30 18:20:13 +0900

    [Silver III] Title: N과 M (4), Time: 124 ms, Memory: 21640 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+    private static int [] arr;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1];
+
+        backTracking(0);
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int index) {
+        if(index == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if(index>0 && arr[index-1] > i) continue;
+            arr[index] = i;
+            backTracking(index+1);
+        }
+
+    }
+}
