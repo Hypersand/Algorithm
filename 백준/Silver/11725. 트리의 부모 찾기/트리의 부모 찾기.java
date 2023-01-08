06f8535982ac90146bd502484e97d029096bb173
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-01-08 14:21:59 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2112 ms, Memory: 82480 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,59 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static boolean [] visited;
+    private static List<Integer>[] list;
+    private static int [] ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        list = new List[N + 1];
+        visited = new boolean[N + 1];
+        ans = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new LinkedList<>();
+        }
+
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        dfs(1);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(ans[i]);
+        }
+
+
+    }
+
+    private static void dfs(int start) {
+        if (!visited[start]) {
+            visited[start] = true;
+            for (int a : list[start]) {
+                if (!visited[a]) {
+                    ans[a] = start;
+                    dfs(a);
+                }
+            }
+        }
+
+
+    }
+
+}
