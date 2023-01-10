00574694a8709dc0aaf4c8151c70b6554a8553e4
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-01-10 12:03:52 +0900

    [Silver I] Title: 곱셈, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+
+        System.out.println(DAC(A,B,C));
+    }
+
+    private static long DAC(long  a, long  b, long  c) {
+       if(b == 0) return 1;
+       else if (b == 1) {
+           return a % c;
+       }
+       else {
+           long  tmp = DAC(a, b / 2, c)%c;
+           if (b % 2 == 0) {
+               return (tmp*tmp)%c;
+           }
+           else {
+               return (tmp * tmp % c) * a % c;
+           }
+       }
+    }
+}
