27ed48fbd4f7377f5b8caafa979ae47269deba96
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-01-18 00:09:21 +0900

    [Long 타입으로 변환]

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,10 +1,11 @@
+package Baekjoon.Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Problem13305 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -14,17 +15,17 @@ public class Main {
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N - 1; i++) {
-            arr[i] = new drive(0, Integer.parseInt(st.nextToken()));
+            arr[i] = new drive(0, Long.parseLong(st.nextToken()));
         }
 
         arr[N - 1] = new drive(0, 0);
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i].price = Integer.parseInt(st.nextToken());
+            arr[i].price = Long.parseLong(st.nextToken());
         }
 
-        int min = arr[0].price;
-        int total = 0;
+        long min = arr[0].price;
+        long total = 0;
         for (int i = 0; i < N-1; i++) {
             min = Math.min(arr[i].price, min);
             total += arr[i].distance * min;
@@ -36,10 +37,10 @@ public class Main {
     }
 
     private static class drive {
-        int price;
-        int distance;
+        long price;
+        long distance;
 
-        public drive(int price, int distance) {
+        public drive(long price, long distance) {
             this.price = price;
             this.distance = distance;
         }
