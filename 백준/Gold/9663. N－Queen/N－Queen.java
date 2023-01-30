41075b61f6be38d0e20ea04ca76cbe11c5a2b1e5
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-01-30 12:02:15 +0900

    [Gold IV] Title: N-Queen, Time: 2404 ms, Memory: 12032 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,47 +1,44 @@
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
 
-    private static int [] arr;
-    private static int count = 0;
+
+    private static int visit_y[] = new int[16];
+    private static int visit_l[] = new int[33];
+    private static int visit_r[] = new int[33];
     private static int N;
+    private static int count = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        backTracking(0);
+
+        nQueen(0);
+
         System.out.println(count);
     }
 
-    private static void backTracking(int index) {
-        if(index == N) {
+    private static void nQueen(int col) {
+
+        if (col == N) {
             count++;
             return;
         }
+
         for (int i = 0; i < N; i++) {
-            arr[index] = i;
-            if(possible(index)) {
-                backTracking(index+1);
+            if (visit_y[i] == 0 && visit_l[col - i + N] == 0 && visit_r[col + i] == 0) {
+                visit_y[i] = 1;
+                visit_l[col-i+N] = 1;
+                visit_r[col + i] = 1;
+                nQueen(col + 1);
+                visit_y[i] = 0;
+                visit_l[col-i+N] = 0;
+                visit_r[col + i] = 0;
             }
         }
-    }
-
-    private static boolean possible(int index) {
-
-        for (int i = 0; i < index; i++) {
-            if(arr[i]==arr[index]) {
-                return false;
-            }
-            else if(Math.abs(i-index)==Math.abs(arr[i]-arr[index])) {
-                return false;
-            }
-        }
-        return true;
     }
 }
