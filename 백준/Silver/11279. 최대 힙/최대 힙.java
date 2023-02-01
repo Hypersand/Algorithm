69dd7dd0e8609eda1bd6e92e3b8f9d16da36d247
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-02-01 13:17:20 +0900

    [Silver II] Title: 최대 힙, Time: 296 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -11,21 +12,21 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         int N = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(br.readLine());
-            if(n>0) queue.add(n);
-            else {
-                if(queue.isEmpty()) sb.append(0 + "\n");
-                else {
-                    sb.append(queue.peek()+"\n");
-                    queue.poll();
+            int tmp = Integer.parseInt(br.readLine());
+            if (tmp == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append("\n");
                 }
-
+                else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            }
+            else {
+                queue.add(tmp);
             }
         }
 
         System.out.println(sb);
-
     }
 }
