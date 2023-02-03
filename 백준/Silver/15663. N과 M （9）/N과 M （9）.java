a480d1a230fdde39b49aaddb05248af8b5544a84
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-02-03 14:26:02 +0900

    [Silver II] Title: N과 M (9), Time: 316 ms, Memory: 62840 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,63 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int M;
+    private static int [] arr;
+    private static int [] arr2;
+    private static boolean [] visited;
+    private static Map<String, Integer> map = new HashMap<>();
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        arr2 = new int[N];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        backTracking(0);
+
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int length) {
+        if (length == M) {
+            String str = "";
+            for (int i = 0; i < M; i++) {
+                str += arr2[i] + " ";
+            }
+            if (!map.containsKey(str)) {
+                map.put(str, 1);
+                sb.append(str);
+                sb.append("\n");
+            }
+        }
+        else {
+            for (int i = 0; i < arr.length; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    arr2[length] = arr[i];
+                    backTracking(length+1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
