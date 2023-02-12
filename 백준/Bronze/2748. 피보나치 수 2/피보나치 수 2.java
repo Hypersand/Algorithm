1897aabb4af2e52fae217dc9d940eab6de7af911
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-02-12 11:55:38 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[91];
+        long[] dp = new long[n + 1];
         dp[0] = 0;
         dp[1] = 1;
+
+
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
