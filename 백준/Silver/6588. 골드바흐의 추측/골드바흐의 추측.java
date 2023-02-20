3faf6e771d644d396924d7aa344ed46fac44ee96
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-02-20 15:08:14 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 456 ms, Memory: 54052 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,43 +1,45 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        boolean[] isPrime = new boolean[1000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        boolean[] isPrime = new boolean[1000000];
         Arrays.fill(isPrime, true);
         isPrime[0] = false;
         isPrime[1] = false;
-        for (int i = 2; i <= Math.sqrt(1000001); i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= isPrime.length-1; j += i) {
-                    isPrime[j] = false;
-                }
+
+        for (int i = 2; i < Math.sqrt(1000000); i++) {
+            for (int j = i * 2; j < 1000000; j += i) {
+                isPrime[j] = false;
             }
         }
 
-        int n = 0;
-        while ((n =sc.nextInt()) != 0) {
+
+        String str = "";
+        while (!(str = br.readLine()).equals("0")) {
+            int n = Integer.parseInt(str);
             int count = 0;
-            for (int i = 2; i<=n/2; i++) {
-                if (isPrime[i]&&isPrime[n - i]) {
-                    System.out.println(n + " = " + i + " + " + (n - i));
+            for (int i = 3; i <= n / 2; i+=2) {
+                if (isPrime[i] && isPrime[n - i]) {
+                    sb.append(n + " = " + i + " + " + (n - i)).append("\n");
                     count++;
                     break;
                 }
             }
-            if (count == 0) {
-                System.out.println("Goldbach's conjecture is wrong.");
-            }
 
+            if (count == 0) {
+                sb.append("Goldbach's conjecture is wrong.").append("\n");
+            }
         }
 
+        System.out.println(sb);
 
     }
 }
