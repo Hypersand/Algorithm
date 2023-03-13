9ff63b13d619d6d9cb420595b6d33aaadecf9900
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-03-13 22:12:41 +0900

    [Silver V] Title: 다리 놓기, Time: 100 ms, Memory: 12360 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -7,24 +7,34 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
-        int[][] dp = new int[31][31];
-        dp[0][0] = 1;
+        int[][] arr = new int[30][30];
+        arr[0][0] = 1;
 
-        for (int i = 1; i <= 30; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= i; j++) {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        for (int i = 1; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                if (j == 0) {
+                    arr[i][j] = 1;
+                    continue;
+                }
+                if (i == j) {
+                    arr[i][j] = 1;
+                    continue;
+                }
+
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
             }
         }
 
         for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            System.out.println(dp[M][N]);
+            System.out.println(arr[M][N]);
         }
+
     }
 }
