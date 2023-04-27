7a3ae8d1b3533ab47168d6705710e4da84bd4bff
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-04-27 09:48:46 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,34 +1,50 @@
 import java.util.*;
 
 class Solution {
+    //배포는 하루에 한번만 가능
+    //
     public int[] solution(int[] progresses, int[] speeds) {
-        Stack<Integer> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        int [] day = new int [progresses.length];
-                
-        for(int i = 0; i<progresses.length; i++) {
-            int a = 100 - progresses[i];
-            if(a%speeds[i]>0) {
-                day[i] = a / speeds[i] + 1;
+        int[] days = new int[speeds.length];
+
+        
+        for (int i = 0; i<days.length; i++) {
+            int tmp = 100 - progresses[i];
+            int remaining = 0;
+            if(tmp%speeds[i]==0) {
+                remaining = tmp / speeds[i];
             }
             else {
-                day[i] = a / speeds[i];
+                remaining = tmp / speeds[i] + 1;
             }
-            System.out.println(day[i]);
-            
-            if(!stack.isEmpty() && stack.peek()>=day[i]) {
-                list.set(stack.size()-1, list.get(stack.size()-1)+1);
-            }
-            else {
-                stack.push(day[i]);
-                // System.out.println(queue.peek());
-                list.add(1);
-            }
+            days[i] = remaining;
         }
         
-        int [] arr = list.stream()
-		              .mapToInt(Integer::intValue)
-                      .toArray();
-        return arr;
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        
+        queue.add(days[0]);
+        
+        for (int i = 1; i<days.length; i++) {
+            if(days[i] > queue.peek()) {
+                list.add(queue.size());
+                queue.clear();
+                queue.add(days[i]);
+            }
+            else {
+                queue.add(days[i]);
+            }
+            
+            if(i == days.length - 1) {
+                    list.add(queue.size());
+                }
+            
+        }
+        
+        int[] ans = new int[list.size()];
+        for (int i = 0; i<ans.length; i++) {
+            ans[i] = list.get(i);
+        }
+        
+        return ans;
     }
 }
