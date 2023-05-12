4a46ed4f78df8bbde41b4c6a6d96c7594991b09f
Hypersand <109746210+Hypersand@users.noreply.github.com>
2023-05-12 10:11:03 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -7,42 +7,25 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int [] arr = new int[N];
 
-        for(int i = 0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int[] coins = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
+        int cnt = 0;
 
-        int i = 0;
-        int count = 0;
-        while (true) {
-            int min = Integer.MAX_VALUE;
-            int newK = 0;
-            for(int j = 0; j<arr.length; j++) {
-
-                if(arr[j]>K) {
-                    break;
-                }
-                if(min > K/arr[j]) {
-                    min = K/arr[j];
-                    newK = K%arr[j];
-                }
-
-            }
-            count += min;
-            K = newK;
-            if(newK == 0) {
-                System.out.println(count);
-                break;
-            }
+        for (int i = N - 1; i >= 0; i--) {
+            cnt += K / coins[i];
+            K %= coins[i];
         }
 
-
-
+        System.out.println(cnt);
     }
 }
