e484e4e3d4eacf3a78e5eaa4e7f255867e898770
Hypersand <on8214@gmail.com>
2023-05-27 18:54:10 +0900

    [Gold V] Title: 행복 유치원, Time: 724 ms, Memory: 71328 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -0,0 +1,44 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            list.add(arr[i] - arr[i - 1]);
+        }
+
+        Collections.sort(list);
+
+        int ans = 0;
+
+        for (int i = 0; i < N - K; i++) {
+            ans += list.get(i);
+        }
+
+        System.out.println(ans);
+
+    }
+}
