7e3fb218e0bdbcc32315117e1a5f262fe25cb72a
Hypersand <on8214@gmail.com>
2023-12-13 10:57:21 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 632 ms, Memory: 161164 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.java
@@ -1,41 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+    private static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[M][N];
-        List<Integer>[] lists = new ArrayList[M];
+        arr = new int[M][N];
         for (int i = 0; i < M; i++) {
-            Set<Integer> set = new HashSet<>();
             st = new StringTokenizer(br.readLine());
+            List<Integer> list = new ArrayList<>();
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                set.add(arr[i][j]);
+                list.add(Integer.parseInt(st.nextToken()));
             }
-            lists[i] = new ArrayList<>(set);
-            Collections.sort(lists[i]);
-        }
-
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Collections.binarySearch(lists[i], arr[i][j]);
-            }
+            binarySearch(list, i);
         }
 
         int cnt = 0;
-        for (int i = 0; i < M; i++) {
+        //i번째 우주와 j번째 우주를 비교
+        for (int i = 0; i < M - 1; i++) {
             for (int j = i + 1; j < M; j++) {
-                if (Arrays.equals(arr[i], arr[j])) {
+                boolean flag = true;
+                for (int k = 0; k < N; k++) {
+                    if (arr[i][k] != arr[j][k]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
                     cnt++;
                 }
             }
         }
+
         System.out.println(cnt);
+    }
+
+    private static void binarySearch(List<Integer> list, int idx) {
+        for (int i = 0; i < list.size(); i++) {
+            int start = 0;
+            int end = list.size() - 1;
+            int target = list.get(i);
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (list.get(mid) <= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            arr[idx][i] = end;
+        }
     }
 }
