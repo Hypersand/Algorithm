3a1d9c4ae928df074ba59c31f1f5fb6893e810a7
Hypersand <on8214@gmail.com>
2023-12-14 11:50:42 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 25.95 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,55 +1,61 @@
 import java.util.*;
-import java.util.Map.Entry;
 class Solution {
     private static Map<String, Integer> map;
-    private static boolean[] used;
-    public List<String> solution(String[] orders, int[] course) {
-        List<String> list = new ArrayList<>();
+    public String[] solution(String[] orders, int[] course) {
+        //각 주문별 알파벳 오름차순 정렬
         for (int i = 0; i<orders.length; i++) {
             char[] arr = orders[i].toCharArray();
             Arrays.sort(arr);
-            orders[i] = String.valueOf(arr);
+            String order = "";
+            for (int j = 0; j<arr.length; j++) {
+                order += arr[j];
+            }
+            orders[i] = order;
         }
+        
+        List<String> list = new ArrayList<>();
+        
+        //코스 길이만큼 각 주문 조합
         for (int i = 0; i<course.length; i++) {
             map = new HashMap<>();
             for (int j = 0; j<orders.length; j++) {
-                used = new boolean[orders[j].length()];
-                select(course[i], 0, 0, "", orders[j].split(""));
-            }
-            Queue<String> queue = new LinkedList<>();
-            int max = 0;
-            for(Entry<String,Integer> entry : map.entrySet()){
-                max = Math.max(max,entry.getValue());   
+                String[] orderArr = orders[j].split("");
+                comb(orderArr, course[i], 0, "");
             }
             
-            for(Entry<String,Integer> entry : map.entrySet()) {
-                if(max >=2 && entry.getValue() == max) {
-                    list.add(entry.getKey());
+            int maxSize = 0;
+            for (int value : map.values()) {
+                maxSize = Math.max(value, maxSize);
+            }
+            
+            if (maxSize < 2) continue;
+            
+            for (String str : map.keySet()) {
+                if(map.get(str) == maxSize) {
+                    list.add(str);
                 }
             }
         }
         
         Collections.sort(list);
-        return list;
+        String[] answer = new String[list.size()];
+        for (int i = 0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        } 
+        
+        return answer;
     }
     
-    public static void select(int maxSize, int size, int idx, String tmp, String[] arr) {
-        if (size == maxSize) {
-            char[] charArr = tmp.toCharArray();
-            Arrays.sort(charArr);
-            tmp = String.valueOf(charArr);
-            map.put(tmp, map.getOrDefault(tmp, 0) + 1);
+    private static void comb(String[] orderArr, int maxSize, int idx, String str) {
+        if (str.length() == maxSize) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
             return;
         }
         
-        for (int i = idx; i<arr.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                select(maxSize, size + 1, i + 1, tmp + arr[i], arr);
-                used[i] = false;
-            }
-            
+        for (int i = idx; i<orderArr.length; i++) {
+            comb(orderArr, maxSize, i + 1, str + orderArr[i]);
         }
+        
         
     }
 }
