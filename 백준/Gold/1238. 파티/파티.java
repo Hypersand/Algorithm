30cc37b3c81e2a3acad7b1acef36151ec6a764c4
Hypersand <on8214@gmail.com>
2023-12-17 15:02:06 +0900

    [Gold III] Title: 파티, Time: 2296 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final int INF = 100000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N + 1][N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            map[start][end] = cost;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j] = INF;
+                }
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (map[i][k] + map[k][j] < map[i][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i == X) continue;
+            max = Math.max(map[i][X] + map[X][i], max);
+        }
+
+        System.out.println(max);
+    }
+}
