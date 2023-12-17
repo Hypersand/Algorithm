4361fdd41a51dc23ebed2defe8e2da8cb14c902d
Hypersand <on8214@gmail.com>
2023-12-17 15:30:41 +0900

    [Gold III] Title: 파티, Time: 152 ms, Memory: 16912 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,49 +1,77 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static final int INF = 100000000;
+    private static int X;
+    private static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N + 1][N + 1];
+        X = Integer.parseInt(st.nextToken());
+        List<Node>[] lists = new List[N + 1];
+        List<Node>[] reverseLists = new List[N + 1];
+        for (int i = 1; i <= N; i++) {
+            lists[i] = new ArrayList<>();
+            reverseLists[i] = new ArrayList<>();
+        }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            map[start][end] = cost;
+            lists[start].add(new Node(end, cost));
+            reverseLists[end].add(new Node(start, cost));
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = INF;
-                }
-            }
-        }
-
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    if (map[i][k] + map[k][j] < map[i][j]) {
-                        map[i][j] = map[i][k] + map[k][j];
-                    }
-                }
-            }
-        }
-
+        int[] dist = dijkstra(lists);
+        int[] reverseDist = dijkstra(reverseLists);
         int max = 0;
         for (int i = 1; i <= N; i++) {
-            if (i == X) continue;
-            max = Math.max(map[i][X] + map[X][i], max);
+            max = Math.max(dist[i] + reverseDist[i], max);
         }
 
         System.out.println(max);
+    }
+
+
+    private static int[] dijkstra(List<Node>[] lists) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(X, 0));
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+        dist[X] = 0;
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+            for (Node nextNode : lists[curNode.end]) {
+                if (dist[nextNode.end] > dist[curNode.end] + nextNode.cost) {
+                    dist[nextNode.end] = dist[curNode.end] + nextNode.cost;
+                    pq.add(new Node(nextNode.end, dist[nextNode.end]));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    private static class Node implements Comparable<Node> {
+        int end;
+        int cost;
+
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
     }
 }
