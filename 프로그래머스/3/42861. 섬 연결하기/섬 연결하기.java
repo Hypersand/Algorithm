16b5d77f626b51b63f70782e17635ba880a6a7a2
Hypersand <on8214@gmail.com>
2023-12-21 18:29:48 +0900

    [level 3] Title: 섬 연결하기, Time: 0.33 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    private static int[] parents;
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+        parents = new int[n];
+        
+        for (int i = 0; i < n; i++)
+            parents[i] = i;
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[2] - o2[2];
+            }
+        });
+        
+        for (int i = 0; i < costs.length; i++) {
+            if(find(costs[i][0]) != find(costs[i][1])) { 
+                answer += costs[i][2];
+                union(costs[i][0], costs[i][1]);
+            }
+        }
+        return answer;
+    }
+    
+    public int find(int node) {
+        if (parents[node] == node)
+            return node;
+        return find(parents[node]);
+    }
+    public void union(int node1, int node2) {
+        int p1 = find(node1);
+        int p2 = find(node2);
+        
+        if (p1 < p2)
+            parents[p2] = p1;
+        else
+            parents[p1] = p2;
+    } 
+}
