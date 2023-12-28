986f50fa71ea81839d53e122a18bc43a675b0de8
Hypersand <on8214@gmail.com>
2023-12-28 12:07:59 +0900

    [level 3] Title: 섬 연결하기, Time: 0.31 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,39 +2,48 @@ import java.util.*;
 class Solution {
     private static int[] parents;
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        parents = new int[n];
-        
-        for (int i = 0; i < n; i++)
-            parents[i] = i;
-        Arrays.sort(costs, new Comparator<int[]>() {
+        parents = new int[n+1];
+        Arrays.sort(costs, new Comparator<>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[2] - o2[2];
+            public int compare(int[] costs1, int[] costs2) {
+                return costs1[2] - costs2[2];
             }
         });
         
-        for (int i = 0; i < costs.length; i++) {
-            if(find(costs[i][0]) != find(costs[i][1])) { 
-                answer += costs[i][2];
-                union(costs[i][0], costs[i][1]);
+        //각 노드들에 대해서 자기 자신을 부모로 설정
+        for (int i = 0; i<costs.length; i++) {
+            parents[costs[i][0]] = costs[i][0];
+            parents[costs[i][1]] = costs[i][1];
+        }
+        
+        int answer = 0;
+        for (int i = 0; i<costs.length; i++) {
+            int A = costs[i][0];
+            int B = costs[i][1];
+            int cost = costs[i][2];
+            if (find(A) != find(B)) {
+                union(A, B);
+                answer += cost;
             }
         }
+        
         return answer;
     }
     
-    public int find(int node) {
-        if (parents[node] == node)
-            return node;
-        return parents[node] = find(parents[node]);
+    private static int find(int node) {
+        int next = parents[node];
+        if (node == next) return node;
+        return parents[node] = find(next);
     }
-    public void union(int node1, int node2) {
-        int p1 = find(node1);
-        int p2 = find(node2);
-        
-        if (p1 < p2)
-            parents[p2] = p1;
-        else
-            parents[p1] = p2;
-    } 
+    
+    private static void union(int A, int B) {
+        int parentsA = find(A);
+        int parentsB = find(B);
+        if (parentsA < parentsB) {
+            parents[parentsB] = parentsA;
+            return;
+        } 
+        parents[parentsA] = parentsB;
+        return;
+    }
 }
