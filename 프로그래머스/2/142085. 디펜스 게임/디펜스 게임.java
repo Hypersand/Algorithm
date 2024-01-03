5e175ba0f660a79beddf5e87a823d0866b31f284
Hypersand <on8214@gmail.com>
2024-01-03 11:44:30 +0900

    [level 2] Title: 디펜스 게임, Time: 0.54 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,28 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
+        if (k >= enemy.length) {
+            return enemy.length;
+        }
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        if (k > enemy.length) return enemy.length;
         for (int i = 0; i<k; i++) {
             pq.add(enemy[i]);
-        }    
-        int answer = k;
+        }
+        int stage = k;
         for (int i = k; i<enemy.length; i++) {
-            //우선순위큐의 가장 작은 수보다 클 경우
-            if (!pq.isEmpty() && pq.peek() < enemy[i]) {
-                int tmp = pq.poll();
+            if (pq.peek() < enemy[i]) {
+                n -= pq.poll();
                 pq.add(enemy[i]);
-                n -= tmp;
             } else {
                 n -= enemy[i];
             }
-            
-            if (n < 0) {
-                break;
-            } else {
-                answer++;
-            }
+            if (n < 0) break;
+            stage++;
         }
-        return answer;
+        
+        return stage;
     }
 }
