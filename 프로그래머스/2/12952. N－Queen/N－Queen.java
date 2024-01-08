fa39cc9b829b8d510dc70354f8e5e0d8a2436999
Hypersand <on8214@gmail.com>
2024-01-08 15:50:39 +0900

    [level 2] Title: N-Queen, Time: 206.27 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,43 +1,40 @@
 import java.util.*;
 class Solution {
+    private static int[] pos;
     private static int N;
-    private static int[] visited; //방문한 열, 행 체크
-    private static int cnt = 0;
+    private static int cnt;
     public int solution(int n) {
-        visited = new int[n];
-        for (int i = 0; i<n; i++) {
-            visited[i] = -1;
-        }
+        pos = new int[n];
         N = n;
-        game(0);
+        move(0);
         return cnt;
     }
     
-    private static void game(int row) {
-        if (row == N) {
+    private static void move(int idx) {
+        if (idx == N) {
             cnt++;
             return;
         }
         
         for (int i = 0; i<N; i++) {
-            visited[row] = i;
-            if (validate(row, i)) {
-                game(row + 1);
+            if (validate(idx, i)) {
+                pos[idx] = i;
+                move(idx + 1);
             }
         }
-        
     }
     
+    //열, 대각선 검증
     private static boolean validate(int row, int col) {
         for (int i = 0; i<row; i++) {
-            //이미 같은 열에 놓았으면 false 반환
-            if (visited[i] == visited[row]) return false;
-            //이미 같은 대각선에 놓았으면 false 반환
-            if (Math.abs((i - row)) == Math.abs(visited[i] - col)) {
+            //열 검증
+            if (pos[i] == col) return false;
+            //대각선 검증
+            if (Math.abs(i - row) - Math.abs(pos[i] - col) == 0) {
                 return false;
             }
         }
         
         return true;
     }
- }
+}
