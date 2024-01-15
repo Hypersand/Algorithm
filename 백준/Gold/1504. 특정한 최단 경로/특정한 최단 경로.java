c1c1b215b8835e857b52b762d48db5b16c7c466f
Hypersand <on8214@gmail.com>
2024-01-15 12:57:29 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 808 ms, Memory: 69892 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,37 +1,35 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static final int INF = 100000000;
-    private static List<Node>[] lists;
-    private static int[] dp;
     private static int N;
+    private static List<Node>[] lists;
+    private static final int INF = 200000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
         lists = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             lists[i] = new ArrayList<>();
         }
-        int E = Integer.parseInt(st.nextToken());
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            lists[u].add(new Node(v, cost));
-            lists[v].add(new Node(u, cost));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            lists[a].add(new Node(b, c));
+            lists[b].add(new Node(a, c));
         }
+
         st = new StringTokenizer(br.readLine());
         int v1 = Integer.parseInt(st.nextToken());
         int v2 = Integer.parseInt(st.nextToken());
         int cost1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N);
         int cost2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N);
-
         if (cost1 >= INF && cost2 >= INF) {
             System.out.println(-1);
         } else {
@@ -40,30 +38,30 @@ public class Main {
     }
 
     private static int dijkstra(int start, int end) {
-        dp = new int[N + 1];
-        Arrays.fill(dp, INF);
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
-        dp[start] = 0;
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            if (dp[node.num] < node.cost) continue;
-            for (Node next : lists[node.num]) {
-                if (dp[next.num] > dp[node.num] + next.cost) {
-                    dp[next.num] = dp[node.num] + next.cost;
-                    pq.add(next);
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        int[] dists = new int[N+1];
+        Arrays.fill(dists, INF);
+        dists[start] = 0;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            for (Node next : lists[node.idx]) {
+                if (dists[next.idx] > dists[node.idx] + next.cost) {
+                    dists[next.idx] = dists[node.idx] + next.cost;
+                    queue.add(new Node(next.idx, dists[next.idx]));
                 }
             }
         }
-        return dp[end];
+
+        return dists[end];
     }
 
     private static class Node implements Comparable<Node> {
-        private int num;
-        private int cost;
+        int idx;
+        int cost;
 
-        public Node(int num, int cost) {
-            this.num = num;
+        public Node(int idx, int cost) {
+            this.idx = idx;
             this.cost = cost;
         }
 
