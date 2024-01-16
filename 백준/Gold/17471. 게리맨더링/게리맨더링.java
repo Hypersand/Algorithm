3829175e87de8777a5b6dcfb3ff52a7cf79e5e9c
Hypersand <on8214@gmail.com>
2024-01-16 13:07:08 +0900

    [Gold IV] Title: 게리맨더링, Time: 116 ms, Memory: 14908 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -7,80 +7,78 @@ public class Main {
     private static int N;
     private static int[] populations;
     private static List<Integer>[] lists;
-    private static int[] group;
-    private static int min = 10000000;
+    private static int[] group; //1번그룹과 2번그룹으로 구분
+    private static int answer = Integer.MAX_VALUE;
+    private static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        populations = new int[N + 1];
         lists = new ArrayList[N + 1];
+        populations = new int[N + 1];
         group = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            lists[i] = new ArrayList<>();
-        }
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             populations[i] = Integer.parseInt(st.nextToken());
+            lists[i] = new ArrayList<>();
         }
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             int num = Integer.parseInt(st.nextToken());
             for (int j = 0; j < num; j++) {
-                int u = Integer.parseInt(st.nextToken());
-                lists[i].add(u);
+                int node = Integer.parseInt(st.nextToken());
+                lists[i].add(node);
+                lists[node].add(i);
             }
         }
-
-        divide(1);
-
-        if (min == 10000000) {
+        comb(1);
+        if (answer == Integer.MAX_VALUE) {
             System.out.println(-1);
-        } else {
-            System.out.println(min);
+            return;
         }
+        System.out.println(answer);
     }
 
-    private static void divide(int idx) {
+    private static void comb(int idx) {
         if (idx == N + 1) {
-            //2개의 그룹으로 나눴으면 각 그룹 검증
-            boolean[] visited = new boolean[N + 1];
+            visited = new boolean[N + 1];
             List<Integer> list = new ArrayList<>();
             for (int i = 1; i <= N; i++) {
                 if (!visited[i]) {
-                    int pops = bfs(visited, i);
-                    list.add(pops);
+                    list.add(bfs(i));
                 }
             }
 
-            //cnt가 2이면 두 개의 그룹으로 나누어짐
             if (list.size() == 2) {
-                min = Math.min(Math.abs(list.get(0) - list.get(1)), min);
+                answer = Math.min(answer, Math.abs(list.get(0) - list.get(1)));
             }
             return;
         }
 
         for (int i = 1; i <= 2; i++) {
             group[idx] = i;
-            divide(idx + 1);
+            comb(idx + 1);
         }
     }
 
-    private static int bfs(boolean[] visited, int num) {
+    private static int bfs(int num) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(num);
         visited[num] = true;
         int groupNum = group[num];
-        int pops = populations[num];
+        int population = populations[num];
         while (!queue.isEmpty()) {
             int node = queue.poll();
             for (int next : lists[node]) {
                 if (!visited[next] && group[next] == groupNum) {
                     queue.add(next);
                     visited[next] = true;
-                    pops += populations[next];
+                    population += populations[next];
                 }
             }
         }
-        return pops;
+        return population;
     }
+
+
+
 }
