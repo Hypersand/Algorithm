834e0551a81e928d6afbcc008c852c0726e13d9a
Hypersand <on8214@gmail.com>
2024-01-19 12:52:41 +0900

    [Gold IV] Title: 좋은 친구, Time: 268 ms, Memory: 46516 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.java
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -12,27 +12,26 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-        int[] nums = new int[21];
+        int[] lens = new int[21];
         for (int i = 0; i < N; i++) {
             arr[i] = br.readLine().length();
         }
 
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i <= K; i++) {
-            if (i == N) break;
             queue.add(arr[i]);
-            nums[arr[i]]++;
+            lens[arr[i]]++;
         }
 
         long answer = 0;
         int idx = K + 1;
         while (!queue.isEmpty()) {
-            int num = queue.poll();
-            nums[num]--;
-            answer += nums[num];
+            int length = queue.poll();
+            lens[length]--;
+            answer += lens[length];
             if (idx < N) {
-                nums[arr[idx]]++;
-                queue.add(arr[idx++]);
+                queue.add(arr[idx]);
+                lens[arr[idx++]]++;
             }
         }
         System.out.println(answer);
