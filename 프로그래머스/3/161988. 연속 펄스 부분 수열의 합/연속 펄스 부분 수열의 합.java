9eb13e24e71fe5761efdc8b8617ecd7f6e6c3e9f
Hypersand <on8214@gmail.com>
2024-01-19 19:47:56 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 28.43 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -6,23 +6,16 @@ class Solution {
         dp1[0] = sequence[0];
         dp2[0] = sequence[0] * (-1);
         long max = Math.max(dp1[0], dp2[0]);
+        int[] multiple1 = {1, -1};
+        int[] multiple2 = {-1, 1};
         for (int i = 1; i<sequence.length; i++) {
             //홀수 번째 수
-            if (i % 2 == 1) {
-                long num1 = dp1[i-1] + sequence[i] * (-1);
-                long num2 = sequence[i] * (-1);
-                dp1[i] = Math.max(num1, num2);
-                long num3 = dp2[i-1] + sequence[i];
-                long num4 = sequence[i];
-                dp2[i] = Math.max(num3, num4);
-            } else {
-                long num1 = dp1[i-1] + sequence[i];
-                long num2 = sequence[i];
-                dp1[i] = Math.max(num1, num2);
-                long num3 = dp2[i-1] + sequence[i] * (-1);
-                long num4 = sequence[i] * (-1);
-                dp2[i] = Math.max(num3, num4);
-            }
+            long num1 = dp1[i-1] + sequence[i] * multiple1[i%2];
+            long num2 = sequence[i] * multiple1[i%2];
+            long num3 = dp2[i-1] + sequence[i] * multiple2[i%2];
+            long num4 = sequence[i] * multiple2[i%2];
+            dp1[i] = Math.max(num1, num2);
+            dp2[i] = Math.max(num3, num4);
             max = Math.max(max, Math.max(dp1[i], dp2[i]));
         }
         return max;
