7b1da116bd3ddc76e6f9ac882b444549f94b160e
Hypersand <on8214@gmail.com>
2024-01-24 15:11:35 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 136 ms, Memory: 12620 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.java
@@ -7,35 +7,47 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static int N, M, L;
+    private static List<Point> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        List<Point> stars = new ArrayList<>();
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
-            stars.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            list.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        if (K == 1) {
+            System.out.println(0);
+            return;
         }
 
         int answer = 0;
-        for (int i = 0; i < K; i++) {
-            int x = stars.get(i).x;
-            for (int j = 0; j < K; j++) {
-                int y = stars.get(j).y;
-                //x와 y를 좌상단으로 하는 트램펄린을 만들었다 가정
-                int cnt = 0;
-                for (Point p : stars) {
-                    if (p.x >= x && p.y >= y && p.x <= x + L && p.y <= y + L) {
-                        cnt++;
-                    }
-                }
-                answer = Math.max(answer, cnt);
+        for (int i = 0; i < list.size() - 1; i++) {
+            Point p1 = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                Point p2 = list.get(j);
+                answer = Math.max(answer, validate(p1.x, p1.y));
+                answer = Math.max(answer, validate(p2.x, p2.y));
+                answer = Math.max(answer, validate(p1.x, p2.y));
+                answer = Math.max(answer, validate(p2.x, p1.y));
             }
         }
 
-        System.out.println(K - answer);
+        System.out.println(list.size() - answer);
+    }
+
+    private static int validate(int x, int y) {
+        int cnt = 0;
+        for (Point p : list) {
+            if (p.x >= x && p.y >= y && p.x <= x + L && p.y <= y + L) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
