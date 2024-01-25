022c6d2419ece1875613710e7c72bea5e4c43449
Hypersand <on8214@gmail.com>
2024-01-25 14:23:52 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.40 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,31 +1,19 @@
 import java.util.*;
 class Solution {
     public long solution(int[] weights) {
-        Arrays.sort(weights);
+        long answer = 0;
         long[] originWeights = new long[1001];
         long[] multipleWeights = new long[4001];
-        
-        long answer = 0;
+        Arrays.sort(weights);
         for (int i = 0; i<weights.length; i++) {
-            int A = weights[i];
-            int B = weights[i] * 2;
-            int C = weights[i] * 3;
-            int D = weights[i] * 4;
-            if (originWeights[A] > 0) {
-                answer += originWeights[A];
-                answer += multipleWeights[B] - originWeights[A];
-                answer += multipleWeights[C] - originWeights[A];
-                answer += multipleWeights[D] - originWeights[A];
-            } else {
-                answer += multipleWeights[B]; 
-                answer += multipleWeights[C];
-                answer += multipleWeights[D];
-            }
-            
-            originWeights[A]++;
-            multipleWeights[B]++;
-            multipleWeights[C]++;
-            multipleWeights[D]++;
+            answer += originWeights[weights[i]];
+            answer += multipleWeights[weights[i]*2] - originWeights[weights[i]];
+            answer += multipleWeights[weights[i]*3] - originWeights[weights[i]];
+            answer += multipleWeights[weights[i]*4] - originWeights[weights[i]];
+            originWeights[weights[i]]++;
+            multipleWeights[weights[i] * 2]++;
+            multipleWeights[weights[i] * 3]++;
+            multipleWeights[weights[i] * 4]++;
         }
         return answer;
     }
