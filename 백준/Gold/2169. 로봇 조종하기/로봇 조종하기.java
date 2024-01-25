db35d43795584105d1fe6f582b1202e6026c795f
Hypersand <on8214@gmail.com>
2024-01-25 13:29:24 +0900

    [Gold II] Title: 로봇 조종하기, Time: 532 ms, Memory: 88556 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -1,50 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N+1][M+2];
-        int[][] dp = new int[N+1][M+2];
-        for (int i = 1; i <= N; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n+1][m+2];
+        int[][] dp = new int[n+1][m+2];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= M; j++) {
+            for (int j = 1; j <= m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dp[1][1] = arr[1][1];
-        for (int i = 2; i <= M; i++) {
+        for (int i = 1; i <= m; i++) {
             dp[1][i] = dp[1][i - 1] + arr[1][i];
         }
 
-        for (int i = 2; i <= N; i++) {
-            int[][] tmp = new int[2][M+2];
-            tmp[0][0] = dp[i - 1][1];
-            tmp[1][M+1] = dp[i - 1][M];
-            for (int j = 1; j <= M; j++) {
-                tmp[0][j] = Math.max(dp[i - 1][j], tmp[0][j - 1]) + arr[i][j];
+        for (int i = 2; i <= n; i++) {
+            int[][] tmpDp = new int[2][m + 2];
+            tmpDp[0][0] = dp[i - 1][1];
+            tmpDp[1][m+1] = dp[i - 1][m];
+            for (int j = 1; j <= m; j++) {
+                tmpDp[0][j] = Math.max(dp[i - 1][j], tmpDp[0][j - 1]) + arr[i][j];
+            }
+            for (int j = m; j >= 1; j--) {
+                tmpDp[1][j] = Math.max(dp[i - 1][j], tmpDp[1][j + 1]) + arr[i][j];
             }
 
-            for (int j = M; j > 0; j--) {
-                tmp[1][j] = Math.max(dp[i - 1][j], tmp[1][j + 1]) + arr[i][j];
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = Math.max(tmpDp[0][j], tmpDp[1][j]);
             }
-
-            for (int j = 1; j <= M; j++) {
-                dp[i][j] = Math.max(tmp[0][j], tmp[1][j]);
-            }
-
         }
 
-        System.out.println(dp[N][M]);
+        System.out.println(dp[n][m]);
     }
-
 }
