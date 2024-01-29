a8d108210bc60b3ed45d26a248696e24cd8d26a6
Hypersand <on8214@gmail.com>
2024-01-29 14:48:36 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 84 ms, Memory: 12100 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        arr = new int[3];
         visited = new boolean[61][61][61];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
@@ -24,35 +24,17 @@ public class Main {
 
     private static int bfs() {
         Queue<Node> queue = new LinkedList<>();
-        if (arr.length == 3) {
-            queue.add(new Node(arr[0], arr[1], arr[2], 0));
-            visited[arr[0]][arr[1]][arr[2]] = true;
-        } else if (arr.length == 2) {
-            queue.add(new Node(arr[0], arr[1], 0, 0));
-            visited[arr[0]][arr[1]][0] = true;
-        } else {
-            queue.add(new Node(arr[0], 0, 0, 0));
-            visited[arr[0]][0][0] = true;
-        }
+        queue.add(new Node(arr[0], arr[1], arr[2], 0));
 
         while (!queue.isEmpty()) {
             Node node = queue.poll();
             for (int i = 0; i < 6; i++) {
-                int nx = 0;
-                int ny = 0;
-                int nz = 0;
-                for (int j = 0; j < arr.length; j++) {
-                    if (j == 0) {
-                        nx = node.x - dir[i][0];
-                        if (nx < 0) nx = 0;
-                    } else if (j == 1) {
-                        ny = node.y - dir[i][1];
-                        if (ny < 0) ny = 0;
-                    } else {
-                        nz = node.z - dir[i][2];
-                        if (nz < 0) nz = 0;
-                    }
-                }
+                int nx = node.x - dir[i][0];
+                int ny = node.y - dir[i][1];
+                int nz = node.z - dir[i][2];
+                if (nx < 0) nx = 0;
+                if (ny < 0) ny = 0;
+                if (nz < 0) nz = 0;
 
                 if (visited[nx][ny][nz]) continue;
                 if (nx <= 0 && ny <= 0 && nz <= 0) {
