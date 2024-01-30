cf8bffb721576d37d4a7c0f2f16a0aebb07e0085
Hypersand <on8214@gmail.com>
2024-01-30 11:19:47 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 820 ms, Memory: 240632 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -1,58 +1,59 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-        int[] lens = new int[N];
-
+        int[] arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        dp[0] = arr[0];
-        Arrays.fill(lens, 1);
-        int length = 1;
-        for (int i = 1; i < N; i++) {
-            if (arr[i] > dp[length - 1]) {
-                dp[length] = arr[i];
-                lens[i] = ++length;
-                continue;
-            }
-
-            int start = 0;
-            int end = length;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (dp[mid] >= arr[i]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
+        int[] dp = new int[N + 1];
+        int[] mem = new int[N + 1];
+        int lis = 1;
+        dp[lis] = arr[1];
+        mem[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            if (arr[i] > dp[lis]) {
+                dp[++lis] = arr[i];
+                mem[i] = lis;
+            } else {
+                int start = 1;
+                int end = lis;
+                while (start <= end) {
+                    int mid = (start + end) / 2;
+                    if (arr[i] <= dp[mid]) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
                 }
+                dp[start] = arr[i];
+                mem[i] = start;
             }
-            dp[start] = arr[i];
-            lens[i] = (start + 1);
         }
+
         StringBuilder sb = new StringBuilder();
-        sb.append(length).append("\n");
         Stack<Integer> stack = new Stack<>();
-        for (int i = N - 1; i >= 0; i--) {
-            if (lens[i] == length) {
+        int length = lis;
+        for (int i = N; i >= 1; i--) {
+            if (length == 0) break;
+            if (mem[i] == length) {
                 stack.push(arr[i]);
                 length--;
             }
         }
 
+        sb.append(lis).append("\n");
         while (!stack.isEmpty()) {
             sb.append(stack.pop()).append(" ");
         }
-
         System.out.println(sb);
+
     }
 }
