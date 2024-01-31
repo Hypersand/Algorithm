bd713bebdffa59665fdb9aa2d5fd5220cce0ebec
Hypersand <on8214@gmail.com>
2024-01-31 20:45:49 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 428 ms, Memory: 40444 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,47 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int[][] arr;
-    private static int[][] dist;
     private static int n;
+    private static int[][] map;
+    private static int[][] dp;
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
-    private static int max = 0;
+    private static int answer = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new int[n][n];
-        dist = new int[n][n];
+        map = new int[n][n];
+        dp = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(dfs(i, j), max);
+                answer = Math.max(answer, dfs(i, j));
             }
         }
-
-        System.out.println(max);
+        System.out.println(answer);
     }
 
     private static int dfs(int x, int y) {
-        if (dist[x][y] > 0) return dist[x][y];
-        dist[x][y] = 1;
+        if (dp[x][y] > 0) return dp[x][y];
+        dp[x][y] = 1;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-            if (arr[nx][ny] <= arr[x][y]) continue;
-            dist[x][y] = Math.max(dist[x][y], dfs(nx, ny) + 1);
+            if (map[nx][ny] <= map[x][y]) continue;
+            dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
         }
 
-        return dist[x][y];
+        return dp[x][y];
     }
 }
