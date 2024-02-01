eed269618685de06934ae86345fd439ae0145bad
Hypersand <on8214@gmail.com>
2024-02-02 00:44:19 +0900

    [Gold IV] Title: 게리맨더링, Time: 100 ms, Memory: 13980 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -7,15 +7,15 @@ public class Main {
     private static int N;
     private static int[] populations;
     private static List<Integer>[] lists;
-    private static boolean[] isGroupA;
-    private static boolean[] visited;
-    private static int answer = 1000000000;
+    private static int[] groups;
+    private static final int INF = 1000000000;
+    private static int answer = INF;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         populations = new int[N + 1];
         lists = new ArrayList[N + 1];
-        isGroupA = new boolean[N + 1];
+        groups = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             populations[i] = Integer.parseInt(st.nextToken());
@@ -24,64 +24,70 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            for (int j = 1; j <= num; j++) {
-                lists[i].add(Integer.parseInt(st.nextToken()));
+            int c = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < c; j++) {
+                int node = Integer.parseInt(st.nextToken());
+                lists[i].add(node);
+                lists[node].add(i);
             }
         }
 
-        comb(1);
+        for (int i = 1; i <= N / 2; i++) {
+            comb(1, 0, i);
+        }
 
-        if (answer == 1000000000) {
+        if (answer == INF) {
             System.out.println(-1);
         } else {
             System.out.println(answer);
         }
     }
 
-    private static void comb(int idx) {
-        if (idx == N + 1) {
-            visited = new boolean[N + 1];
+    private static void comb(int idx, int cnt, int maxCnt) {
+        if (cnt == maxCnt) {
+            boolean[] visited = new boolean[N + 1];
             List<Integer> list = new ArrayList<>();
             for (int i = 1; i <= N; i++) {
                 if (!visited[i]) {
-                    list.add(search(i));
+                    list.add(bfs(i, visited));
                 }
             }
 
             if (list.size() == 2) {
-                answer = Math.min(Math.abs(list.get(0) - list.get(1)), answer);
+                answer = Math.min(answer, Math.abs(list.get(0) - list.get(1)));
             }
             return;
         }
 
-        for (int i = 1; i <= 2; i++) {
-            if (i == 1) {
-                isGroupA[idx] = true;
-            } else {
-                isGroupA[idx] = false;
-            }
-            comb(idx + 1);
+
+        for (int i = idx; i <= N; i++) {
+            groups[i] = 1;
+            comb(i + 1, cnt + 1, maxCnt);
+            groups[i] = 0;
         }
+
+
+
 
     }
 
-    private static int search(int node) {
+    private static int bfs(int start, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
-        boolean group = isGroupA[node];
-        visited[node] = true;
-        queue.add(node);
-        int population = populations[node];
+        int groupNum = groups[start];
+        int population = populations[start];
+        queue.add(start);
+        visited[start] = true;
         while (!queue.isEmpty()) {
             int cur = queue.poll();
             for (int next : lists[cur]) {
-                if (isGroupA[next] == group && !visited[next]) {
+                if (!visited[next] && groups[next] == groupNum) {
                     visited[next] = true;
-                    population += populations[next];
                     queue.add(next);
+                    population += populations[next];
                 }
             }
         }
+
         return population;
     }
 }
