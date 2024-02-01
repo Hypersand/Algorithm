09d30fb19f241a0af99cd14f697c8008d4b15302
Hypersand <on8214@gmail.com>
2024-02-01 14:13:12 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 112 ms, Memory: 12568 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -6,71 +6,47 @@ import java.util.*;
 import java.util.List;
 
 public class Main {
-    private static List<Point> stores;
-    private static Queue<Node> queue;
-    private static Point endP;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            queue = new LinkedList<>();
-            stores = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(br.readLine());
-            queue.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), 20));
+            Point start = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            List<Point> stores = new ArrayList<>();
             for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(br.readLine());
                 stores.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
             }
             st = new StringTokenizer(br.readLine());
-            endP = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            Point end = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 
-            if (bfs()) {
-                sb.append("happy").append("\n");
+            if (canArrived(start, end, stores)) {
+                System.out.println("happy");
             } else {
-                sb.append("sad").append("\n");
+                System.out.println("sad");
             }
         }
-
-        System.out.println(sb);
     }
 
-    private static boolean bfs() {
+    private static boolean canArrived(Point start, Point end, List<Point> stores) {
+        Queue<Point> queue = new LinkedList<>();
         boolean[] visited = new boolean[stores.size()];
+        queue.add(start);
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            int endDist = (Math.abs(node.x - endP.x) + Math.abs(node.y - endP.y));
-            int endBeerCnt = endDist / 50;
-            if (endDist % 50 > 0) endBeerCnt++;
-            if (endBeerCnt <= node.beerCnt) {
+            Point p = queue.poll();
+            if ((double)(Math.abs(end.x - p.x) + Math.abs(end.y - p.y)) / 50 <= 20) {
                 return true;
             }
-            for (int i = 0; i < stores.size(); i++) {
+            for (int i = 0; i<stores.size(); i++) {
+                Point store = stores.get(i);
                 if (visited[i]) continue;
-                int dist = Math.abs(node.x - stores.get(i).x) + Math.abs(node.y - stores.get(i).y);
-                int requiredBeerCnt = dist / 50;
-                if (dist % 50 > 0) {
-                    requiredBeerCnt++;
+                if ((double)(Math.abs(store.x - p.x) + Math.abs(store.y - p.y)) / 50 <= 20) {
+                    visited[i] = true;
+                    queue.add(store);
                 }
-                if (node.beerCnt < requiredBeerCnt) continue;
-                visited[i] = true;
-                queue.add(new Node(stores.get(i).x, stores.get(i).y, 20));
             }
         }
-
         return false;
-    }
-
-    private static class Node {
-        private int x;
-        private int y;
-        private int beerCnt;
-
-        public Node(int x, int y, int beerCnt) {
-            this.x = x;
-            this.y = y;
-            this.beerCnt = beerCnt;
-        }
     }
 }
