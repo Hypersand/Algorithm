bac8d147d1080c3b8099dd74c3c90b9563da4092
Hypersand <on8214@gmail.com>
2024-03-05 00:06:16 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 296 ms, Memory: 37088 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,41 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int N,K;
-    public static int[] A;
-    public static int[] nums = new int[100001];
-
     public static void main(String[] args) throws IOException {
-        input();
-        process();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-    public static void input() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        A = new int[N+1];
-        
-        st = new StringTokenizer(br.readLine(), " ");
-        for(int i=1; i<=N; i++) A[i] = Integer.parseInt(st.nextToken());
-    }
-
-    public static void process() {
-        int R=0, cnt = 0;
-        
-        for(int L=1; L<=N; L++) {
-            nums[A[L-1]]--;
-
-            while(R+1<=N && nums[A[R+1]] < K) {
-                nums[A[++R]]++;
-            }
-
-            cnt = Math.max(cnt, R-L+1);
+        if (N == 1) {
+            System.out.println(1);
+            return;
         }
 
-        System.out.println(cnt);
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] nums = new int[100001];
+
+        int answer = 1;
+        int start = 0;
+        int end = 1;
+        nums[arr[start]]++;
+        while (end < N) {
+            nums[arr[end]]++;
+
+            while (nums[arr[end]] > K && start < end) {
+                nums[arr[start]]--;
+                start++;
+            }
+
+            answer = Math.max(answer, end - start + 1);
+            end++;
+        }
+
+        System.out.println(answer);
     }
 }
