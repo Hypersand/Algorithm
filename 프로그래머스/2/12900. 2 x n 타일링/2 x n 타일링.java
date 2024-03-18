02e7f42621a8555d56338c696393a69841d44e1d
Hypersand <on8214@gmail.com>
2024-03-18 10:08:00 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.43 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    private static final int INF = 1000000007;
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % INF;
+        }
+        
+        return dp[n];
+    }
+}
