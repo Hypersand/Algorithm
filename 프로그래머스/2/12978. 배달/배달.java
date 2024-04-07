90a9fa0c20bb36fe021889b164e7e0b9a6e12ee9
Hypersand <on8214@gmail.com>
2024-04-07 22:44:12 +0900

    [level 2] Title: 배달, Time: 0.66 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    private static List<Node>[] lists;
+    private static int[] dists;
+    private static final int INF = 1000000000;
+    public int solution(int N, int[][] road, int K) {
+        dists = new int[N+1];
+        Arrays.fill(dists, INF);
+        lists = new ArrayList[N+1];
+        for (int i = 1; i<=N; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i<road.length; i++) {
+            int start = road[i][0];
+            int end = road[i][1];
+            int dist = road[i][2];
+            lists[start].add(new Node(end, dist));
+            lists[end].add(new Node(start, dist));
+        }
+        
+        dijkstra();
+        int answer = 0;
+        for (int i = 1; i<=N; i++) {
+            if (dists[i] <= K) answer++;
+        }
+
+
+        return answer;
+    }
+    
+    private static void dijkstra() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        dists[1] = 0;
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            for (Node node : lists[num]) {
+                if (dists[node.x] > dists[num] + node.dist) {
+                    dists[node.x] = dists[num] + node.dist;
+                    queue.add(node.x);
+                }
+            }
+        }
+    }
+    
+    private static class Node {
+        private int x;
+        private int dist;
+        
+        public Node(int x, int dist) {
+            this.x = x;
+            this.dist = dist;
+        }
+    }
+}
