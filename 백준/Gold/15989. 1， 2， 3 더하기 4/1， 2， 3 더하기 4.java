67e59708de0b3e4113c878aa6c23f585c16c0ac0
Hypersand <on8214@gmail.com>
2024-04-18 17:57:19 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 200 ms, Memory: 88648 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] dp = new int[n + 5][4];
+            dp[1][1] = 1;
+            dp[2][1] = 1;
+            dp[2][2] = 1;
+            dp[3][1] = 1;
+            dp[3][2] = 1;
+            dp[3][3] = 1;
+            for (int j = 4; j <= n; j++) {
+                dp[j][1] = dp[j - 1][1];
+                dp[j][2] = dp[j - 2][1] + dp[j - 2][2];
+                dp[j][3] = dp[j - 3][1] + dp[j - 3][2] + dp[j - 3][3];
+            }
+
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
