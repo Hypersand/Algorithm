fc024f28448734ed7715896baa6061a78b49d792
Hypersand <on8214@gmail.com>
2024-05-13 15:58:56 +0900

    [Gold IV] Title: 팰린드롬?, Time: 752 ms, Memory: 243340 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -12,28 +12,34 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            dp[i][i] = 1;
+            if (arr[i] == arr[i - 1]) {
+                dp[i - 1][i] = 1;
+            }
+        }
+
+        for (int i = 2; i <= N - 1; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                if (arr[j] == arr[j + i] && dp[j + 1][j + i - 1] == 1) {
+                    dp[j][j + i] = 1;
+                }
+            }
+        }
+
         int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            boolean flag = true;
-            while (start < end) {
-                if (arr[start] == arr[end]) {
-                    start++;
-                    end--;
-                    continue;
-                }
-                flag = false;
-                break;
-            }
-
-            if (!flag) {
-                sb.append(0).append("\n");
+            if (dp[start][end] == 1) {
+                sb.append(1).append("\n");
                 continue;
             }
-            sb.append(1).append("\n");
+            sb.append(0).append("\n");
         }
 
         System.out.println(sb);
