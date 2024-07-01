115732eab5b0b432d89df18d3cb26e0507f81076
Hypersand <on8214@gmail.com>
2024-07-01 11:36:05 +0900

    [Gold V] Title: 개똥벌레, Time: 272 ms, Memory: 29508 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,50 +1,41 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int[] sums = new int[H + 1];
+        for (int i = 0; i < N; i++) {
+            int height = Integer.parseInt(br.readLine());
+            // 석순
+            if (i % 2 == 0) {
+                sums[0]++;
+                sums[height]--;
+                continue;
+            }
+            // 종유석
+            sums[H - height]++;
+            sums[H]--;
+        }
 
-	static int N, H;
+        int min = sums[0];
+        int count = 1;
 
-	public static void main(String[] args) throws Exception {
+        for (int i = 1; i < sums.length - 1; i++) {
+            sums[i] += sums[i - 1];
+            if (sums[i] < min) {
+                min = sums[i];
+                count = 1;
+            } else if (sums[i] == min) {
+                count++;
+            }
+        }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] input = br.readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		H = Integer.parseInt(input[1]);
-
-		int[] bot = new int[H + 2];
-		int[] top = new int[H + 2];
-
-		for (int i = 0; i < N / 2; i++) {
-			bot[Integer.parseInt(br.readLine())]++;
-			top[H - Integer.parseInt(br.readLine()) + 1]++;
-		}
-
-		for (int i = 1; i <= H; i++) {
-			bot[i] += bot[i - 1];
-		}
-
-		for (int i = H; i >= 1; i--) {
-			top[i] += top[i + 1];
-		}
-
-		int min = N;
-		int cnt = 0;
-
-		for (int i = 1; i <= H; i++) {
-			int obs = (bot[H] - bot[i - 1]) + (top[1] - top[i + 1]);
-
-			if (obs < min) {
-				min = obs;
-				cnt = 1;
-			} else if (obs == min)
-				cnt++;
-		}
-
-		bw.write(min + " " + cnt + "\n");
-		bw.flush();
-
-	}
-
+        System.out.println(min + " " + count);
+    }
 }
