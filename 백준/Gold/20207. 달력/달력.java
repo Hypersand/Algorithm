0779f0bb5d53c6c71e2a4bf2397ff361cfc95c6c
Hypersand <on8214@gmail.com>
2024-07-01 19:13:05 +0900

    [Gold V] Title: 달력, Time: 116 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -1,28 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[1001];
+
+        int[] arr = new int[367];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            for (int j = start; j <= end; j++) {
-                arr[j]++;
-            }
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s]++;
+            arr[e + 1]--;
         }
 
         int sum = 0;
         int width = 0;
         int height = 0;
-        for (int i = 1; i <= 1000; i++) {
+        for (int i = 1; i <= 366; i++) {
+            arr[i] += arr[i - 1];
             if (arr[i] != 0) {
                 width++;
                 height = Math.max(height, arr[i]);
@@ -34,8 +33,6 @@ public class Main {
             height = 0;
         }
 
-
         System.out.println(sum);
     }
-
 }
