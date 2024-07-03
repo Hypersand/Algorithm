a6b1dc18d3f96ac4d081c8282e3fe236100b920f
Hypersand <on8214@gmail.com>
2024-07-03 09:21:01 +0900

    [Gold IV] Title: 미로 탈출, Time: 1052 ms, Memory: 196200 KB -BaekjoonHub

--- 백준/Gold/14923. 미로 탈출/미로 탈출.java
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,81 +7,99 @@ import java.util.StringTokenizer;
 
 public class Main {
     private static int N, M;
-
-    private static int[][] map;
-    private static boolean[][][] visited; // 0 : 지팡이를 안쓰고 방문, 1 : 지팡이를 쓰고 방문
+    private static int hx, hy, ex, ey;
+    private static int[][] arr;
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
-    private static Point start = null;
-    private static Point end = null;
+    private static int[][][] dists; // [][][0] : 벽을 안뚫고 방문, [][][1] : 벽을 뚫고 방문
+    private static final int INF = 1000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M][2];
 
         st = new StringTokenizer(br.readLine());
-        start = new Point(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1);
+        hx = Integer.parseInt(st.nextToken()) - 1;
+        hy = Integer.parseInt(st.nextToken()) - 1;
 
         st = new StringTokenizer(br.readLine());
-        end = new Point(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1);
+        ex = Integer.parseInt(st.nextToken()) - 1;
+        ey = Integer.parseInt(st.nextToken()) - 1;
 
+        arr = new int[N][M];
+        dists = new int[N][M][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < 2; k++) {
+                    dists[i][j][k] = INF;
+                }
             }
         }
 
-        System.out.println(find());
+        find();
+        int answer = Math.min(dists[ex][ey][0], dists[ex][ey][1]);
+        if (answer == INF) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(answer);
     }
 
-    private static int find() {
+    private static void find() {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(start.x, start.y, 0, 0));
-        visited[start.x][start.y][0] = true;
+        queue.add(new Node(hx, hy, 0, false));
+        dists[hx][hy][0] = 0;
 
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            // 목적지 도착
-            if (cur.x == end.x && cur.y == end.y) return cur.dist;
-
+            Node node = queue.poll();
             for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
+                int nx = node.x + dx[i];
+                int ny = node.y + dy[i];
                 if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-
-                // 벽이고, 아직 벽을 부순적이 없을때
-                if (map[nx][ny] == 1 && cur.isUsedStaff == 0) {
-                    visited[nx][ny][0] = true;
-                    queue.add(new Node(nx, ny, cur.dist + 1, 1));
-                    continue;
-                }
-
-                // 길일때 방문한 적이 없다면
-                if (map[nx][ny] == 0 && !visited[nx][ny][cur.isUsedStaff]) {
-                    visited[nx][ny][cur.isUsedStaff] = true;
-                    queue.add(new Node(nx, ny, cur.dist + 1, cur.isUsedStaff));
+                if (node.passedWall && arr[nx][ny] == 1) continue;
+                if (arr[nx][ny] == 0) {
+                    boolean flag = false;
+                    if (node.passedWall) {
+                        if (dists[nx][ny][1] > node.dist + 1) {
+                            flag = true;
+                            dists[nx][ny][1] = node.dist + 1;
+                        }
+                    } else {
+                        if (dists[nx][ny][0] > node.dist + 1) {
+                            flag = true;
+                            dists[nx][ny][0] = node.dist + 1;
+                        }
+                    }
+                    if (flag) {
+                        queue.add(new Node(nx, ny, node.dist + 1, node.passedWall));
+                    }
+                } else {
+                    if (dists[nx][ny][1] > node.dist + 1) {
+                        dists[nx][ny][1] = node.dist + 1;
+                        queue.add(new Node(nx, ny, node.dist + 1, true));
+                    }
                 }
             }
+
         }
 
-        return -1;
     }
 
     private static class Node {
         private int x;
         private int y;
         private int dist;
-        private int isUsedStaff;
+        private boolean passedWall;
 
-        public Node(int x, int y, int dist, int isUsedStaff) {
+        public Node(int x, int y, int dist, boolean passedWall) {
             this.x = x;
             this.y = y;
             this.dist = dist;
-            this.isUsedStaff = isUsedStaff;
+            this.passedWall = passedWall;
         }
     }
+
 }
