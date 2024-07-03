6579bd6f6610000e01e55c3c1630c5782b0f6fd6
Hypersand <on8214@gmail.com>
2024-07-03 16:14:13 +0900

    [Gold V] Title: 자두나무, Time: 128 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -9,23 +9,41 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
-        int[] orders = new int[T + 1];
+        int[] arr = new int[T + 1];
+        int[][][] dp = new int[T + 1][3][W + 1];
+
         for (int i = 1; i <= T; i++) {
-            orders[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        int max = W;
-        int[][][] dp = new int[T + 1][3][W + 2];
-        for (int i = 1; i <= T; i++) {
-            for (int k = 1; k <= W + 1; k++) {
-                if (orders[i] == 1) {
-                    dp[i][1][k] = Math.max(dp[i - 1][1][k], dp[i - 1][2][k - 1]) + 1;
-                    dp[i][2][k] = Math.max(dp[i - 1][2][k], dp[i - 1][1][k - 1]);
-                } else {
-                    if (i == 1 && k == 1) continue; // 무조건 1번 위치에서 시작하기 때문에, i가 1일 때, k가 1일 수 없다.
-                    dp[i][1][k] = Math.max(dp[i - 1][1][k], dp[i - 1][2][k - 1]);
-                    dp[i][2][k] = Math.max(dp[i - 1][2][k], dp[i - 1][1][k - 1]) + 1;
+
+        dp[1][1][0] = arr[1] == 1 ? 1 : 0;
+        int max = dp[1][1][0];
+        if (W > 0) {
+            dp[1][2][1] = arr[1] == 2 ? 1 : 0;
+            max = Math.max(max, dp[1][2][1]);
+        }
+
+        for (int i = 2; i <= T; i++) {
+            for (int j = 1; j <= 2; j++) {
+                for (int k = 0; k <= W; k++) {
+                    if (i - k <= 0) break;
+                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j][k]);
+                    if (j == 1) {
+                        if (k > 0) {
+                            dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][2][k - 1]);
+                        }
+                    } else {
+                        if (k > 0) {
+                            dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][1][k - 1]);
+                        }
+                    }
+
+                    if (j == arr[i]) {
+                        dp[i][j][k] += 1;
+                    }
+
+                    max = Math.max(dp[i][j][k], max);
                 }
-                max = Math.max(max, Math.max(dp[i][1][k], dp[i][2][k]));
             }
         }
 
