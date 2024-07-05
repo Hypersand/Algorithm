303e8d9e0cbc999642fd8287dc607ac187d82783
Hypersand <on8214@gmail.com>
2024-07-05 11:37:51 +0900

    [Gold IV] Title: 수들의 합 4, Time: 372 ms, Memory: 36448 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -10,22 +10,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] sum = new int[N + 1];
-        Map<Integer, Long> map = new HashMap<>();
-        st = new StringTokenizer(br.readLine());
+        long K = Integer.parseInt(st.nextToken());
+
+        long[] sum = new long[N + 1];
         long answer = 0;
+        Map<Long, Integer> map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             sum[i] = Integer.parseInt(st.nextToken()) + sum[i - 1];
             if (sum[i] == K) answer++;
             if (map.containsKey(sum[i] - K)) {
                 answer += map.get(sum[i] - K);
             }
-
-            map.put(sum[i], map.getOrDefault(sum[i], 0L) + 1);
+            map.put(sum[i], map.getOrDefault(sum[i], 0) + 1);
         }
 
         System.out.println(answer);
-
     }
+
 }
