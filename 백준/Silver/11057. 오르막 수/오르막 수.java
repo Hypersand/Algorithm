86e9dc8485e789094d60599085d2a5f58011115a
Hypersand <on8214@gmail.com>
2024-07-12 14:10:44 +0900

    [Silver I] Title: 오르막 수, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,23 +7,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N + 1][10];
-
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = i+1;
+        for (int i = 0; i <= 9; i++) {
+            dp[1][i] = i + 1;
         }
 
-
         for (int i = 2; i <= N; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
+            dp[i][0] = 1;
+            for (int j = 1; j <= 9; j++) {
                 dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
             }
         }
 
         System.out.println(dp[N][9]);
-
     }
 }
