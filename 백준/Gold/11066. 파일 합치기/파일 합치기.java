85d63772105d118d918285186d74d1abfc83d031
Hypersand <on8214@gmail.com>
2024-07-12 16:20:13 +0900

    [Gold III] Title: 파일 합치기, Time: 604 ms, Memory: 27900 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.java
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.java
@@ -4,30 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final int INF = 100000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             int K = Integer.parseInt(br.readLine());
-            int[] arr = new int[K+1];
-            int[][] dp = new int[K+1][K+1];
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[K + 1];
+            int[] sum = new int[K + 1];
+            int[][] dp = new int[K + 1][K + 1];
             for (int j = 1; j <= K; j++) {
-                arr[j] = Integer.parseInt(st.nextToken()) + arr[j-1];
+                arr[j] = Integer.parseInt(st.nextToken());
+                sum[j] = sum[j - 1] + arr[j];
             }
 
             for (int dist = 1; dist < K; dist++) {
                 for (int start = 1; start + dist <= K; start++) {
-                    int end = start + dist;
-                    dp[start][end] = Integer.MAX_VALUE;
-                    for (int mid = start; mid < end; mid++) {
-                        dp[start][end] = Math.min(dp[start][end], dp[start][mid] + dp[mid + 1][end]);
+                    dp[start][start + dist] = INF;
+                    for (int mid = start; mid < start + dist; mid++) {
+                        dp[start][start + dist] = Math.min(dp[start][mid] + dp[mid + 1][start + dist] + sum[start + dist] - sum[start - 1],
+                                dp[start][start + dist]);
                     }
-                    dp[start][end] += arr[end] - arr[start - 1];
-
                 }
             }
+
             System.out.println(dp[1][K]);
+
+
         }
     }
 }
