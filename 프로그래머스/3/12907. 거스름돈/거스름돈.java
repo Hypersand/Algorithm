a23b4da1580eb453741fdda47658caf4f9a90280
Hypersand <on8214@gmail.com>
2024-07-15 16:24:36 +0900

    [level 3] Title: 거스름돈, Time: 71.82 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,22 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[] money) {
-        int[][] dp = new int[money.length+1][n+1];
-        int answer = 0;
-        
-        for(int i=1; i<money.length+1; i++){
-            for(int j=0; j<n+1; j++){
-                if(j==0){
-                    dp[i][j] = 1;
+        int[][] dp = new int[money.length + 1][n + 1];
+        // i : 거스름돈 종류, j : 원
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                if (j == 0) dp[i][j] = 1;
+                if (j - money[i - 1] < 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j]);
+                    continue;
+                } else if (j - money[i - 1] == 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j] + 1, dp[i][j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i][j], (dp[i - 1][j] + dp[i][j - money[i - 1]]) % 1000000007);
                 }
-                else{
-                    if(j<money[i-1]){
-                        dp[i][j] = dp[i-1][j];
-                    }else{
-                        dp[i][j] = (dp[i-1][j] + dp[i][j-money[i-1]]) % 1000000007;
-                    }
-                }
+                
             }
         }
+        
         return dp[money.length][n];
+        
     }
 }
