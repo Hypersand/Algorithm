9700b105aba917b2264f7ea321f7e7c3d04d66de
Hypersand <on8214@gmail.com>
2024-07-15 09:22:55 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 400 ms, Memory: 41912 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,49 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     private static int n;
     private static int[][] arr;
-    private static int[][] dists;
+    private static int[][] dp;
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
-    private static int max = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
-        dists = new int[n][n];
+        dp = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            Arrays.fill(dp[i], -1);
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        int answer = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                max = Math.max(find(i, j), max);
+                if (dp[i][j] == -1) find(i, j);
+                answer = Math.max(dp[i][j], answer);
             }
         }
 
-        System.out.println(max);
+        System.out.println(answer);
     }
 
     private static int find(int x, int y) {
-        if (dists[x][y] != 0) return dists[x][y];
-
-        dists[x][y] = 1;
+        if (dp[x][y] != -1) return dp[x][y];
+        dp[x][y] = 1;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
-            if (arr[x][y] >= arr[nx][ny]) continue;
-            dists[x][y] = Math.max(dists[x][y], find(nx, ny) + 1);
+            if (arr[nx][ny] <= arr[x][y]) continue;
+            dp[x][y] = Math.max(dp[x][y], find(nx, ny) + 1);
         }
 
-        return dists[x][y];
+        return dp[x][y];
     }
 }
