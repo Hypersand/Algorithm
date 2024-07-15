367ed554a8ef6ce079688a5937d86e99da79a5dd
Hypersand <on8214@gmail.com>
2024-07-15 15:21:19 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 220 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,30 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] dp = new int[N + 1][N + 1];
-        int[][] arr = new int[N + 1][3]; //1행 1열 : 첫번째 행렬의 행, 1행 2열 : 첫번쨰 행렬의 열
+        int[] rows = new int[N + 1];
+        int[] cols = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            arr[i][1] = r;
-            arr[i][2] = c;
+            rows[i] = r;
+            cols[i] = c;
         }
 
         for (int dist = 1; dist < N; dist++) {
-            for (int start = 1; start + dist <= N; start++) {
+            for (int start = 1; start <= N; start++) {
                 int end = start + dist;
-                dp[start][end] = INF;
+                if (end > N) break;
+                dp[start][end] = Integer.MAX_VALUE;
                 for (int mid = start; mid < end; mid++) {
-                    dp[start][end] = Math.min(dp[start][end], dp[start][mid] + dp[mid + 1][end] + arr[start][1] * arr[mid][2] * arr[end][2]);
+                    dp[start][end] = Math.min(dp[start][end], dp[start][mid] + dp[mid + 1][end] +
+                            rows[start] * cols[mid] * cols[end]);
                 }
+
             }
         }
 
