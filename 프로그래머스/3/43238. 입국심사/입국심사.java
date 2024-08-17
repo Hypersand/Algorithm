d43dd5d838d376b1b04a676fb7ec8f50fe924858
Hypersand <on8214@gmail.com>
2024-08-17 21:58:52 +0900

    [level 3] Title: 입국심사, Time: 129.55 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,20 +1,22 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         Arrays.sort(times);
-        long start = times[0];
-        long end = n * (long)times[times.length - 1];
+        long start = 0;
+        long end = times[0] * (long)n;
+        
         while (start <= end) {
             long mid = (start + end) / 2;
-            long cnt = 0;
+            long num = 0;
             for (int i = 0; i<times.length; i++) {
-                cnt += mid / times[i];
+                num += mid / times[i];
             }
             
-            if (cnt >= n) {
-                end = mid - 1;
-            } else {
+            if (num < n) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         
