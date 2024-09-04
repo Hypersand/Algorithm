4656425ba6a2df1c2d6cb2739cce5d4e5c2d578d
Hypersand <on8214@gmail.com>
2024-09-05 01:19:22 +0900

    [Gold V] Title: 트리와 쿼리, Time: 1180 ms, Memory: 80544 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,72 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static List<Integer>[] newLists;
-    private static int[] sizeArr;
+    private static List<Integer>[] lists;
+    private static boolean[] visited;
+    private static int[] answers;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
-        boolean[] visited = new boolean[N + 1];
-        sizeArr = new int[N + 1];
-        visited[R] = true;
-        List<Integer>[] lists = new List[N + 1];
-        newLists = new List[N + 1];
-        for (int i = 1; i < lists.length; i++) {
-            lists[i] = new ArrayList<>();
-            newLists[i] = new ArrayList<>();
+
+        lists = new List[N + 1];
+        visited = new boolean[N + 1];
+        answers = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            lists[i] = new ArrayList();
         }
-        Queue<Integer> queue = new LinkedList<>();
+
+
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int U = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-            lists[U].add(V);
-            lists[V].add(U);
-            if (U == R) {
-                queue.add(V);
-                newLists[U].add(V);
-                visited[V] = true;
-            }
-            if (V == R) {
-                queue.add(U);
-                newLists[V].add(U);
-                visited[U] = true;
-            }
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            lists[u].add(v);
+            lists[v].add(u);
         }
 
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int i = 0;  i < lists[node].size(); i++) {
-                int tmp = lists[node].get(i);
-                if (!visited[tmp]) {
-                    visited[tmp] = true;
-                    newLists[node].add(tmp);
-                    queue.add(tmp);
-                }
-            }
-        }
+        visited[R] = true;
+        find(R);
 
-        countSubTreeNode(R);
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < Q; i++) {
-            int node = Integer.parseInt(br.readLine());
-            sb.append(sizeArr[node]).append("\n");
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(answers[num]);
         }
 
-        System.out.println(sb);
+
     }
 
-    private static void countSubTreeNode(int curNode) {
-        sizeArr[curNode] = 1;
-        for (int node : newLists[curNode]) {
-            countSubTreeNode(node);
-            sizeArr[curNode] += sizeArr[node];
+    private static int find(int node) {
+        for (int cur : lists[node]) {
+            if (!visited[cur]) {
+                visited[cur] = true;
+                answers[node] += find(cur);
+            }
         }
+
+        return answers[node] += 1;
     }
 }
