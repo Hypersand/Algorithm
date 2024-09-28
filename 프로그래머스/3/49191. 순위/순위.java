880535de967b7400150e74a8621410818b349761
Hypersand <on8214@gmail.com>
2024-09-28 13:50:23 +0900

    [level 3] Title: 순위, Time: 22.77 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,49 +1,70 @@
 import java.util.*;
 
 class Solution {
-    private static final int INF = 10000000;
+    private static List<Integer>[] lists;
+    private static List<Integer>[] reverseLists;
+    private static boolean[] visited;
+    private static int N;
     public int solution(int n, int[][] results) {
-        int[][] arr1 = new int[n+1][n+1]; // 위로 올라가는 그래프
-        int[][] arr2 = new int[n+1][n+1]; // 아래로 내려가는 그래프
-        for (int i = 1; i<=n; i++) {
-            Arrays.fill(arr1[i], INF);
-            Arrays.fill(arr2[i], INF);
+        lists = new ArrayList[n + 1];
+        reverseLists = new ArrayList[n + 1];
+        N = n;
+        
+        for (int i = 1; i <= n; i++) {
+            lists[i] = new ArrayList<>();
+            reverseLists[i] = new ArrayList<>();
         }
         
-        
-        for (int i = 0; i<results.length; i++) {
-            int start = results[i][0];
-            int end = results[i][1];
-            arr2[start][end] = 1;
-            arr1[end][start] = 1;
+        for (int i = 0; i < results.length; i++) {
+            int a = results[i][0];
+            int b = results[i][1];
+            lists[a].add(b);
+            reverseLists[b].add(a);
         }
         
-        for (int k = 1; k<=n; k++) {
-            for (int i = 1; i<=n; i++) {
-                for (int j = 1; j<=n; j++) {
-                    if (i == j) continue;
-                    if (arr1[i][k] == 1 && arr1[k][j] == 1) {
-                        arr1[i][j] = 1;
-                    }
-                    
-                    if (arr2[i][k] == 1 && arr2[k][j] == 1) {
-                        arr2[i][j] = 1;
-                    }
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (find(i)) answer++;
+        }
+        
+        return answer;
+    }
+    
+    private static boolean find(int idx) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(idx);
+        visited = new boolean[N + 1];
+        visited[idx] = true;
+        
+        int cnt = 0;
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : lists[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                    cnt++;
                 }
             }
         }
         
-        int answer = 0;
-        for (int i = 1; i<=n; i++) {
-            int cnt = 0;
-            for (int j = 1; j<=n; j++) {
-                if (i == j) continue;
-                if (arr1[i][j] == 1 || arr2[i][j] == 1) cnt++;
+        queue.add(idx);
+        visited = new boolean[N + 1];
+        visited[idx] = true;
+        
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int next : reverseLists[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                    cnt++;
+                }
             }
-            
-            if (cnt == n - 1) answer++;
         }
         
-        return answer;
+        if (cnt == N - 1) return true;
+        
+        return false;
     }
 }
