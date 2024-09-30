e06b4f8302de06f23265569e76f4b3a1aada84c5
Hypersand <on8214@gmail.com>
2024-09-30 10:01:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 51.82 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,54 +3,52 @@ import java.util.*;
 class Solution {
     private static List<Integer>[] lists;
     private static int[] dists;
-    private static boolean[] visited;
     public int solution(int n, int[][] edge) {
         lists = new ArrayList[n + 1];
-        dists = new int[n+1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i<=n; i++) {
+        dists = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             lists[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i<edge.length; i++) {
-            int start = edge[i][0];
-            int end = edge[i][1];
-            lists[start].add(end);
-            lists[end].add(start);
+        for (int i = 0; i < edge.length; i++) {
+            int a = edge[i][0];
+            int b = edge[i][1];
+            lists[a].add(b);
+            lists[b].add(a);
         }
         
-        bfs();
-        Arrays.sort(dists);
-        int max = dists[n];
-        int cnt = 0;
-        for (int i = n; i>=0; i--) {
-            if (max == dists[i]) {
-                cnt++;
+        find();
+        
+        int cnt = 1;
+        int maxDist = 0;
+        for (int i = 2; i <= n; i++) {
+            if (dists[i] > maxDist) {
+                maxDist = dists[i];
+                cnt = 1;
                 continue;
             }
             
-            if (max < dists[i]) {
-                cnt = 1;
-                max = dists[i];
+            if (dists[i] == maxDist) {
+                cnt++;
             }
         }
         
         return cnt;
     }
     
-    private static void bfs() {
+    private static void find() {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
-        visited[1] = true;
+        dists[1] = 1;
+        
         while(!queue.isEmpty()) {
             int cur = queue.poll();
             for (int next : lists[cur]) {
-                if (!visited[next]) {
-                    visited[next] = true;
-                    dists[next] = dists[cur] + 1;
-                    queue.add(next);
-                }
+                if (dists[next] != 0) continue;
+                dists[next] = dists[cur] + 1;
+                queue.add(next);
             }
         }
+        
     }
 }
