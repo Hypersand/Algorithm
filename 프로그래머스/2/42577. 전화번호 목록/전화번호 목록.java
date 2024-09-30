0a780d85e1398c8725992f33ea484c2ed7c4611c
Hypersand <on8214@gmail.com>
2024-09-30 11:13:01 +0900

    [level 2] Title: 전화번호 목록, Time: 270.28 ms, Memory: 191 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,22 +2,13 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Arrays.sort(phone_book, (s1, s2) -> s1.length() - s2.length());
+        
         Set<String> set = new HashSet<>();
-        int[] nums = new int[21];
-        
-        for (int i = 0; i< phone_book.length; i++) {
-            nums[phone_book[i].length()]++;
-        }
-        
-        for (int i = 0; i< phone_book.length; i++) {
+        for (int i = 0; i < phone_book.length; i++) {
             for (int j = 1; j <= phone_book[i].length(); j++) {
-                if (nums[j] != 0) {
-                    String str = phone_book[i].substring(0, j);
-                    if (!set.isEmpty() && set.contains(str)) {
-                        return false;
-                    }
-                }
+                String key = phone_book[i].substring(0, j);
+                if (set.contains(key)) return false;
             }
             set.add(phone_book[i]);
         }
