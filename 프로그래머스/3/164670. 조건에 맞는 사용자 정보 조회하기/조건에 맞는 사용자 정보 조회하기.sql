6160541e4e3ea5065af2f343d6a0bf6ee1d16e0d
Hypersand <on8214@gmail.com>
2024-10-02 00:07:39 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,16 +1,13 @@
--- 중고 거래 게시물을 3건 이상 등록한 사용자
-SELECT 
-  USER_ID, 
-  NICKNAME, 
-  CONCAT(CITY, CONCAT(' ', CONCAT(STREET_ADDRESS1, CONCAT(' ', STREET_ADDRESS2)))) "전체주소",
-  SUBSTR(TLNO, 1, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8, 4) "전화번호"
-FROM USED_GOODS_USER
-WHERE USER_ID IN (
-    SELECT B.WRITER_ID
-    FROM USED_GOODS_BOARD B
-    JOIN USED_GOODS_USER U
-    ON B.WRITER_ID = U.USER_ID
-    GROUP BY B.WRITER_ID
-    HAVING COUNT(B.BOARD_ID) >= 3
-)
-ORDER BY USER_ID DESC;
+-- 중고 거래 게시물을 3건 이상 등록한 사용자의 정보 조회
+-- 전체 주소 : 시, 도로명 주소, 상세 주소 함께 출력
+-- 전화번호 : 하이폰 문자열 삽입해서 출력
+SELECT U.USER_ID, 
+    U.NICKNAME, 
+    (U.CITY || ' ' || U.STREET_ADDRESS1 || ' ' || U.STREET_ADDRESS2) "전체주소",
+    substr(U.TLNO, 1, 3) || '-' || substr(U.TLNO, 4, 4) || '-' || substr(U.TLNO, 8, 4) "전화번호"
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_USER U
+ON B.WRITER_ID = U.USER_ID
+GROUP BY U.USER_ID, U.NICKNAME, U.CITY, U.STREET_ADDRESS1, U.STREET_ADDRESS2, TLNO
+HAVING COUNT(U.USER_ID) >= 3
+ORDER BY U.USER_ID DESC;
