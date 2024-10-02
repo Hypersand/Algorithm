3888060edff090a5d19b5b0041502c01ef9b272d
Hypersand <on8214@gmail.com>
2024-10-02 19:44:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.94 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,8 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         List<Job> list = new ArrayList<>();
-        
-        for (int i = 0; i<jobs.length; i++) {
+        for (int i = 0; i < jobs.length; i++) {
             list.add(new Job(jobs[i][0], jobs[i][1]));
         }
         
@@ -15,24 +14,20 @@ class Solution {
         int time = 0;
         int sum = 0;
         int idx = 0;
-        
         while (count < list.size()) {
             while (idx < list.size() && list.get(idx).start <= time) {
-                pq.add(list.get(idx));
-                idx++;
+                pq.add(list.get(idx++));
             }
             
-            //수행할 수 있는 작업이 없다면
             if (pq.isEmpty()) {
                 time = list.get(idx).start;
-            } else {
-                Job job = pq.poll();
-                sum += job.cost + time - job.start;
-                time += job.cost;
-                count++;
+                continue;
             }
             
-            
+            Job job = pq.poll();
+            sum += job.cost + time - job.start;
+            time += job.cost;
+            count++;
         }
         
         return sum / list.size();
@@ -42,17 +37,19 @@ class Solution {
         private int start;
         private int cost;
         
-        public Job(int start, int cost) {
+        private Job(int start, int cost) {
             this.start = start;
             this.cost = cost;
         }
         
-        @Override
         public int compareTo(Job job) {
             if (this.start == job.start) {
                 return this.cost - job.cost;
             }
             return this.start - job.start;
+            
         }
+        
+        
     }
 }
