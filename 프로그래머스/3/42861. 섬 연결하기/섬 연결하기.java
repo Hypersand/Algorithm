6d16c989b3fc5c2ec479b61b0e0b9571620bff1b
Hypersand <on8214@gmail.com>
2024-10-04 01:13:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.70 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,49 +1,65 @@
 import java.util.*;
+
 class Solution {
     private static int[] parents;
     public int solution(int n, int[][] costs) {
-        parents = new int[n+1];
-        Arrays.sort(costs, new Comparator<>() {
-            @Override
-            public int compare(int[] costs1, int[] costs2) {
-                return costs1[2] - costs2[2];
-            }
-        });
-        
-        //각 노드들에 대해서 자기 자신을 부모로 설정
-        for (int i = 0; i<costs.length; i++) {
-            parents[costs[i][0]] = costs[i][0];
-            parents[costs[i][1]] = costs[i][1];
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
         }
         
-        int answer = 0;
-        for (int i = 0; i<costs.length; i++) {
-            int A = costs[i][0];
-            int B = costs[i][1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        for (int i = 0; i < costs.length; i++) {
+            int a = costs[i][0];
+            int b = costs[i][1];
             int cost = costs[i][2];
-            if (find(A) != find(B)) {
-                union(A, B);
-                answer += cost;
+            pq.add(new Edge(a, b, cost));
+        }   
+        
+        int answer = 0;
+        
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (find(edge.a) != find(edge.b)) {
+                union(edge.a, edge.b);
+                answer += edge.cost;
             }
         }
         
         return answer;
     }
     
-    private static int find(int node) {
-        int next = parents[node];
-        if (node == next) return node;
-        return parents[node] = find(next);
+    private static void union(int a, int b) {
+        int pA = find(a);
+        int pB = find(b);
+        
+        if (pA <= pB) {
+            parents[pB] = pA;
+        } else {
+            parents[pA] = pB;
+        }
+        
     }
     
-    private static void union(int A, int B) {
-        int parentsA = find(A);
-        int parentsB = find(B);
-        if (parentsA < parentsB) {
-            parents[parentsB] = parentsA;
-            return;
-        } 
-        parents[parentsA] = parentsB;
-        return;
+    private static int find(int node) {
+        if (parents[node] == node) return parents[node];
+        
+        return parents[node] = find(parents[node]);
+    }
+    
+    private static class Edge implements Comparable<Edge> {
+        private int a;
+        private int b;
+        private int cost;
+        
+        private Edge(int a, int b, int cost) {
+            this.a = a;
+            this.b = b;
+            this.cost = cost;
+        }
+        
+        public int compareTo(Edge edge) {
+            return this.cost - edge.cost;
+        }
     }
 }
