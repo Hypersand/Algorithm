cfa57788a22071b1e755754c0aef3d1ce331ced6
Hypersand <on8214@gmail.com>
2024-10-04 00:38:42 +0900

    [level 2] Title: 구명보트, Time: 10.69 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        if (people.length == 1) return 1;
         Arrays.sort(people);
         int start = 0;
         int end = people.length - 1;
         int answer = 0;
+        
         while (start <= end) {
-            if (start == end) {
-                answer++;
-                break;
-            }
-            
-            if (people[start] + people[end] <= limit) {
-                start++;
+            if (people[start] + people[end] > limit) {
                 end--;
             } else {
+                start++;
                 end--;
             }
             answer++;
