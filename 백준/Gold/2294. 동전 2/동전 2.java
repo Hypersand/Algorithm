1a13e5d8cb0617e8c342c5be92a393f70b80e89b
Hypersand <on8214@gmail.com>
2024-10-04 22:07:17 +0900

    [Gold V] Title: 동전 2, Time: 84 ms, Memory: 11896 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,29 +5,35 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static final int INF = 10000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] coins = new int[n];
-        int[] dp = new int[k + 1];
+        int[] coins = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
 
-        Arrays.fill(dp, Integer.MAX_VALUE-1);
+        Arrays.sort(coins);
+
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, INF);
         dp[0] = 0;
 
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-            for (int j = coins[i]; j <= k; j++) {
-                dp[j] = Math.min(dp[j], dp[j - coins[i]]+1);
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j < coins.length; j++) {
+                if (i - coins[j] < 0) break;
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
             }
         }
 
-        if (dp[k] == Integer.MAX_VALUE || dp[k] == Integer.MAX_VALUE - 1) {
+        if (dp[k] == INF) {
             System.out.println(-1);
-        } 
-        else {
-            System.out.println(dp[k]);
+            return;
         }
+
+        System.out.println(dp[k]);
     }
 }
