76087ae2437b6b7bc0f206b97a78f3ded93a3165
Hypersand <on8214@gmail.com>
2024-10-04 21:10:34 +0900

    [level 3] Title: 등굣길, Time: 0.68 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,30 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] arr = new int[m + 1][n + 1];
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i<puddles.length; i++) {
-            arr[puddles[i][0]][puddles[i][1]] = 1;
+        int[][] dp = new int[n][m];
+        
+        for (int i = 0; i < puddles.length; i++) {
+            dp[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
         }
         
-        for (int i = 1; i<=m; i++) {
-            if (arr[i][1] == 1) break;
-            dp[i][1] = 1;
+        for (int i = 0; i < m; i++) {
+            if (dp[0][i] == -1) break;
+            dp[0][i] = 1;
         }
         
-        for (int i = 1; i<=n; i++) {
-            if (arr[1][i] == 1) break;
-            dp[1][i] = 1;
+        for (int i = 1; i < n; i++) {
+            if (dp[i][0] == -1) break;
+            dp[i][0] = 1;
         }
         
-        for (int i = 1; i<=m; i++) {
-            for (int j = 1; j<=n; j++) {
-                if (arr[i][j] == 1) continue;
-                if (i == 1 || j == 1) continue;
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (dp[i][j] == -1) continue;
+                int a = dp[i - 1][j];
+                int b = dp[i][j - 1];
+                if (a == -1) a = 0;
+                if (b == -1) b = 0;
+                dp[i][j] = (a + b) % 1000000007;
             }
         }
         
-        return dp[m][n];
+        return dp[n - 1][m - 1];
+        
     }
 }
