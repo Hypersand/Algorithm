7d56abacec7c2b3d43be67717bf96ef787368874
Hypersand <on8214@gmail.com>
2024-10-05 00:30:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 12648 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,38 +1,57 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] w = new int[N+1];
-        int[] v = new int[N+1];
-        int[][] dp = new int[N+1][K + 1];
 
-        for (int i = 1; i <= N; i++) {
+        List<Node> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            list.add(new Node(weight, value));
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                if (w[i] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-                }
+        Collections.sort(list);
+
+        int[] dp = new int[K + 1];
+        int max = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = K; j >= 1; j--) {
+                if (j - list.get(i).weight < 0) continue;
+                dp[j] = Math.max(dp[j], dp[j - list.get(i).weight] + list.get(i).value);
+                max = Math.max(dp[j], max);
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(max);
     }
 
+    private static class Node implements Comparable<Node> {
+        private int weight;
+        private int value;
+
+        public Node(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.weight == o.weight) {
+                return o.value - this.value;
+            }
+            return this.weight - o.weight;
+        }
+    }
 }
